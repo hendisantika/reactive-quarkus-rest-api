23471c4fc6ace390e8cc532f2a34982f50f2009f
Hendi Santika <hendisantika@gmail.com>
2021-10-11 15:38:32 +0700

    #7 - Add AccountMapper

---
+++ src/main/java/com/hendisantika/account/AccountMapper.java
@@ -0,0 +1,30 @@
+package com.hendisantika.account;
+
+import org.mapstruct.Mapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : reactive-quarkus-rest-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/10/21
+ * Time: 15.36
+ */
+@Mapper(componentModel = "cdi")
+public interface AccountMapper {
+    AccountDto toDomain(Account entity);
+
+    Account toEntity(Account domain);
+
+    default List<AccountDto> toDomainList(List<Account> list) {
+        return list.stream().map(this::toDomain).collect(Collectors.toList());
+    }
+
+    default List<Account> toEntityList(List<Account> list) {
+        return list.stream().map(this::toEntity).collect(Collectors.toList());
+    }
+}
