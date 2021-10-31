3d3a4cd8fd5a44a12a64868110bbe4250c5a8c9a
Hendi Santika <hendisantika@gmail.com>
2021-10-31 09:12:53 +0700

    #18 - Add flywat migration script

--- src/main/resources/db/migration/V1.0.0__20211011_1543_init.sql
+++ src/main/resources/db/migration/V1.0.0__20211011_1543_init.sql
@@ -3,5 +3,3 @@ CREATE TABLE account
     account_id SERIAL,
     name       TEXT NOT NULL
 );
-INSERT INTO account (account_id, name)
-VALUES (1, 'Test');
---
+++ src/main/resources/db/migration/V2.0.0__20211030_0943_init_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO account (account_id, name)
+VALUES (1, 'Test 1'),
+       (2, 'Test 2'),
+       (3, 'Test 3'),
+       (4, 'Test 4'),
+       (5, 'Test 5'),
+       (6, 'Test 6'),
+       (7, 'Test 7'),
+       (8, 'Test 8'),
+       (9, 'Test 9'),
+       (10, 'Test 10');
